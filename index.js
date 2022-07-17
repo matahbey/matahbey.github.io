fc47a1767fa9ceaf1d367575f72a1d0d9831e531
matahbey <82177280+matahbey@users.noreply.github.com>
2022-07-17 21:08:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -25,8 +25,6 @@ for( let x = 0; x < columns; x++ ) {
 const draw = () => {
 	context.fillStyle = 'rgba(0, 0, 0, 0.05)';
 	context.fillRect(0, 0, canvas.width, canvas.height);
-	ctx.font = "30px Arial";
-        ctx.fillText("Hello World",10,50);
 	context.fillStyle = '#0F0';
 	context.font = fontSize + 'px monospace';
 
@@ -36,6 +34,8 @@ const draw = () => {
 		context.fillText(text, i*fontSize, rainDrops[i]*fontSize);
 		
 		if(rainDrops[i]*fontSize > canvas.height && Math.random() > 0.975){
+			ctx.font = "30px Arial";
+                        ctx.fillText("Hello World",10,50);
 			rainDrops[i] = 0;
         }
 		rainDrops[i]++;
